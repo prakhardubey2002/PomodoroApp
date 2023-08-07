c5215ce67386f784c734922994f53d858a728dff
Prakhar Dubey <prakhardubey2002@gmail.com>
2023-08-07 23:27:42 +0530

    lottijson added,firstscreencompleted

--- App.jsx
+++ App.jsx
@@ -2,30 +2,36 @@
 import { Alert, StyleSheet, Text, View, Image, Button, TouchableOpacity } from 'react-native';
 import React from 'react';
 import { height, width } from './config/dimension';
-
+import LottieView from 'lottie-react-native';
 const App = () => {
   return (
     <View style={styles.main}>
       <Image
         style={styles.background}
         source={{
-          uri: 'https://images.unsplash.com/photo-1641025389903-6d06a65a9c0a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80',
+          uri: 'https://images.unsplash.com/photo-1618172193622-ae2d025f4032?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1064&q=80',
         }}
       />
       <View style={styles.textcontainer} >
-        <Image
-          style={styles.background}
+        {/* <Image
+          style={styles.image}
           source={{
-            uri: 'https://images.unsplash.com/photo-1641025389903-6d06a65a9c0a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80',
+            uri: 'https://raw.githubusercontent.com/prakhardubey2002/PomodoroApp/main/assets/logo.png',
           }}
-        />
-        <Text style={styles.texttitle} >AryaVart</Text>
-        <Text>
+        /> */}
+        <LottieView style={styles.image} source={
+          require
+          ('./assets/lottie/animation_ll167kxj.json')} autoPlay loop />
+        <Text style={styles.texttitle} >Aryavarm</Text>
+        <Text style={styles.text}  >
           "Stay Calm, Boost Productivity: Your Stress-Free Companion with
           Pomodoro!"
         </Text>
         <TouchableOpacity style={styles.button} onPress={() => Alert.alert("Hi")}>
           <Text style={styles.buttonText}>Get started</Text>
+          <LottieView style={styles.bimage} source={
+          require
+          ('./assets/lottie/load.json')} autoPlay loop />
         </TouchableOpacity>
       </View>
 
@@ -52,16 +58,20 @@ const styles = StyleSheet.create({
   textcontainer: {
     position: 'absolute',
     bottom: 50,
-    left: 40
+    left: 40,
+    justifyContent:'center',
+    alignItems:'center',
   },
   button: {
-    backgroundColor: '#fe79b1',
+    backgroundColor: '#9867e1',
+    width:width*0.5,
     paddingHorizontal: 24,
     paddingVertical: 14,
     borderRadius: 32,
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: 12
+    marginTop: 12,
+    flexDirection:'row'
   },
   buttonText: {
     color: 'white',
@@ -76,5 +86,17 @@ const styles = StyleSheet.create({
     textShadowColor: 'rgba(0, 0, 0, 0.5)', // Shadow color (semi-transparent black)
     textShadowOffset: { width: 2, height: 2 }, // Shadow offset (x, y)
     textShadowRadius: 5,
+  },
+  image:{
+    width:250,
+    height:250,
+  },
+  bimage:{
+    height:40,
+    width:50,
+  },
+  text:{
+    fontSize:14,
+    textAlign:'center'
   }
 });
